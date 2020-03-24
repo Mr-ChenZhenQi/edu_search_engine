34281b4341cbb93f9b0fe6f9d514409cd2b098f0
zhangsan <zhangsan@qq.com>
2020-03-24 22:49:59 +0800

    测试

--- dr_search_engine/urls.py
+++ dr_search_engine/urls.py
@@ -21,5 +21,6 @@ from cnblogsSearchEngine import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('home/',views.home,name='home')
+    url('home/',views.home,name='home'),
+    url(r'^search/',views.dosearch,name='search')
 ]
