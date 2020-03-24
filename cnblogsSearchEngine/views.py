439998afd70c4999b8213d045c16032f07db2db0
zhangsan <zhangsan@qq.com>
2020-03-24 22:51:14 +0800

    测试

--- cnblogsSearchEngine/views.py
+++ cnblogsSearchEngine/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+def dosearch(request):
+    keyword=request.GET.get('q','')
+    print(keyword)
+    return render(request,'result.html')
